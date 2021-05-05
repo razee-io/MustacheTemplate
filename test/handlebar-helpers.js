3f3ab9570ec60794d8b55d2df3719a43326e2425
Ramkumar Gowrishankar <ramkumar.gowrishankar@ibm.com>
2021-05-05 09:41:56 -0400

    feat: add support for sha256 handlebar helper (ref #255) (#257)
    * feat: add sha256 handlebar helper
    
    * Not sure if this needs to be commitited
    
    * Revert "Not sure if this needs to be commitited"
    
    This reverts commit 36d91ff973f97d38190aa59f0ed914c4d294430d.
    
    * address code review comments
    
    * check if data is string
    
    * semicolon
    
    * single quote
    
    * single quote
    
    Co-authored-by: Alex Lewitt <48691328+alewitt2@users.noreply.github.com>

--- test/handlebar-helpers.js
+++ test/handlebar-helpers.js
@@ -322,4 +322,12 @@ describe('handlebar-helper', function () {
         ret = HandlebarHelper.includes([11,35,80,120],11,5)
         assert.equal(false, ret, '11 in [11,35,80,120] but indexFrom > arr.length');
     });
+    it('should calculate the sha256 hash', function() {
+        ret = HandlebarHelper.sha256('message')
+        assert.equal(ret, 'ab530a13e45914982b79f9b7e3fba994cfd1f3fb22f71cea1afbf02b460c6d1d', 'sha256 of word message')
+    });
+    it('should check the input for sha256 hash', function() {
+        ret = HandlebarHelper.sha256(356)
+        assert.equal(ret, '', 'sha256 of integer is not calculated')
+    });
 });
